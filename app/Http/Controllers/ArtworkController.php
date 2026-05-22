e2bfe2dffdfa87724d121dc3763708eedb4ad4a4
Ansar Javaid <ansarrjavaid@gmail.com>
2026-05-22 12:38:50 +0500

    changes

--- app/Http/Controllers/ArtworkController.php
+++ app/Http/Controllers/ArtworkController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use SebastianBergmann\CodeCoverage\Util\Percentage;
@@ -11,7 +11,7 @@ use function PHPSTORM_META\map;
 
 class ArtworkController extends Controller
 {
-    public function store(Request $request)
+    public function ex1(Request $request)
     {
         $validated = $request->validate([
             'input' => 'required|array',
@@ -170,10 +170,117 @@ class ArtworkController extends Controller
             ]
         );
     }
-    // logger("data is here " . json_encode($request->all()));       
-    public function ex9(Request $request) {
-        
+
+    public function ex7(Request $request)
+    {
+        $request->validate([
+            'input' => 'required|array',
+            'input.stock' => 'required|integer',
+            'input.requests' => 'required|array',
+        ]);
+
+        $input = $request->input('input');
+        $stock = $input['stock'];
+        $requests = $input['requests'];
+        $remainRequest = [];
+
+        foreach ($requests as $req) {
+            $stock = $stock - $req;
+            if ($stock >= 0) {
+                $remainRequest[] = true;
+            } else {
+                $stock = $stock + $req;
+                $remainRequest[] = false;
+            }
+        }
+        return response()->json([
+            'success' => true,
+            'data' => [$remainRequest],
+            "error" => null
+        ]);
     }
+
+    public function ex8(Request $request)
+    {
+        $request->validate([
+            'input' => 'required|array',
+            'input.ordered' => 'required|numeric|min:0',
+            'input.shipped' => 'required|array',
+        ]);
+        $input = $request['input'];
+        $ordered = $input['ordered'];
+        $shipped = $input['shipped'];
+
+        $quantityLeft = 0;
+        foreach ($shipped as $ship) {
+            if ($ship > 0) {
+                $ordered = $ordered - $ship;
+                $quantityLeft = $ordered;
+            }
+        }
+
+        if ($quantityLeft < 0) {
+            return response()->json([
+                'success' => true,
+                'message' => "No Quantity Left",
+                'error' => null
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'message' => ["remaining :" => $quantityLeft],
+            'error' => null
+        ]);
+    }
+
+    public function ex9(Request $request)
+    {
+        $request->validate([
+            'input' => 'required|array',
+            'input.*.id' => 'required|string',
+            'input.*.time' => 'required|numeric',
+        ]);
+        $input = $request['input'];
+
+        $result = collect($input)->unique("id")->filter()->values()->pluck("id");
+
+        return response()->json([
+            'success' => true,
+            'data' => $result,
+            'error' => null
+        ]);
+    }
+
+    public function ex10(Request $request)
+    {
+        $request->validate([
+            "input" => "required",
+            "input.created_at" => "required|date_format:Y-m-d",
+            "input.valid_days" => "required|numeric",
+            "input.current_date" => "required|date_format:Y-m-d",
+        ]);
+        $input = $request->input('input');
+        $valid_days = $input['valid_days'];
+        $created_at = Carbon::parse($input["created_at"])->format("Y-m-d");
+        $current_date = Carbon::parse($input['current_date'])->format("Y-m-d");
+        $expiration_date = Carbon::parse($created_at)->addDays($valid_days)->format("Y-m-d");
+
+        if (Carbon::parse($current_date)->greaterThan($expiration_date)) {
+            return response()->json([
+                "success" => false,
+                "data" => ["valid" => false],
+                "error" => "The date has expired."
+            ]);
+        } else {
+            return response()->json([
+                "success" => true,
+                "data" => ["valid" => true],
+                "error" => null
+            ]);
+        }
+    }
+
+
     public function ex12(Request $request)
     {
         $request->validate([
@@ -213,11 +320,42 @@ class ArtworkController extends Controller
         ]);
     }
 
-    public function ex13(Request $request) {}
+
+    public function ex13(Request $request)
+    {
+        $request->validate([
+            'input' => 'required|array',
+            'input.guest' => 'array',
+            'input.guest.*.id' => 'required|integer',
+            'input.guest.*.qty' => 'required|integer',
+            'input.user' => 'array',
+            'input.user.*.id' => 'required|integer',
+            'input.user.*.qty' => 'required|integer',
+        ]);
+
+        $input = $request->input('input');
+        $guests = $input['guest'];
+        $users = $input['user'];
+
+        $mergedArray = array_merge($guests, $users);
+        $mergedArray = collect($mergedArray)->groupBy('id')->map(function ($item) {
+            return [
+                'id' => $item[0]['id'],
+                'qty' => $item->sum('qty')
+            ];
+        })->values()->toArray();
+
+        return response()->json([
+            'success' => true,
+            'data' => $mergedArray,
+            'error' => null
+        ]);
+    }
 
 
     public function ex14(Request $request)
     {
+
         $request->validate([
             'input' => 'required|array',
             'input.nums' => 'required|array',
@@ -248,5 +386,7 @@ class ArtworkController extends Controller
         ]);
     }
 
-    public function ex15(Request $request) {}
+    public function ex15(Request $request) {
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtworkController;
 
-Route::post('/exercise-1-artwork-version', [ArtworkController::class, 'store']);
+Route::post('/exercise-1-artwork-version', [ArtworkController::class, 'ex1']);
 Route::post('/exercise-3-cart-validator', [ArtworkController::class, 'ex3']);
 Route::post('/exercise-4-vendor-allocation', [ArtworkController::class, 'ex4']);
 Route::post('/exercise-5-discount', [ArtworkController::class, 'ex5']);
 Route::post('/exercise-6-approval-flow', [ArtworkController::class, 'ex6']);
-
+Route::post('/exercise-7-inventory', [ArtworkController::class, 'ex7']);
+Route::post('/exercise-8-shipment', [ArtworkController::class, 'ex8']);
 Route::post('/exercise-9−webhook', [ArtworkController::class, 'ex9']);
+Route::post('/exercise-10-quote-expiry', [ArtworkController::class, 'ex10']);
 Route::post('/exercise-12-bundle-pricing', [ArtworkController::class, 'ex12']);
 Route::post('/exercise-13-cart-merge', [ArtworkController::class, 'ex13']);
 Route::post('/exercise-14-upsell', [ArtworkController::class, 'ex14']);
